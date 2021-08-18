d940c55adec475684a008633ff57d309dc157e25
Adrien Ehrhardt <UT2WLS@EMEA.CIB>
2021-08-18 09:44:07 +0200

    install qpdf on gitlab, prepare for github action / travis CI, added test structure

---
+++ tests/testthat/test-generate_missingness.R
@@ -0,0 +1,10 @@
+test_that("multiplication works", {
+  n <- 10000
+  mu.X <- c(1, 2, 3)
+  Sigma.X <- matrix(c(9, 3, 2, 3, 4, 0, 2, 0, 1), nrow = 3)
+  X.complete.cont <- MASS::mvrnorm(n, mu.X, Sigma.X)
+  rs <- generate_miss(X.complete.cont, 0.5, mechanism = "MNAR2")
+  expect_equal(length(rs), 3)
+  expect_equal(nrow(rs$X.incomp), n)
+  expect_equal(ncol(rs$X.incomp), 3)
+})
